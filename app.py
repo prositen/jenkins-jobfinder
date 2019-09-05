84079e68039a3b8c53405cc290594fa18f8e1acb
Anna Holmgren <anna.holmgren@feelgood.se>
2019-09-05 11:16:11 +0200

    Optionally filter by name (regex)

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ def hello_world():
     return "Yeah, hi"
 
 
+@app.route('/jobs/<name>')
 @app.route('/jobs')
-def get_jobs():
+def get_jobs(name=''):
     result = list()
     for url in app.config.get('JENKINS_URLS'):
         server = jenkins.Jenkins(url=url, username=app.config.get('JENKINS_USER'),
                                  password=app.config.get('JENKINS_PASSWORD'))
-        jobs = server.get_jobs()
+        jobs = server.get_job_info_regex(name)
         result += [(j.get('name', ''), j.get('url', ''), url) for j in jobs]
 
     jobs = sorted(result, key=lambda x: x[0])
