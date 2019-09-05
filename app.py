263ed159a74a708a05d4bd990cabb63cbfee38c2
Anna Holmgren <anna.holmgren@feelgood.se>
2019-09-05 11:21:08 +0200

    Add form for searching

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import jenkins
 
 app = Flask(__name__)
@@ -14,6 +14,8 @@ def hello_world():
 @app.route('/jobs')
 def get_jobs(name=''):
     result = list()
+    if not name:
+        name = request.args.get('name', '')
     for url in app.config.get('JENKINS_URLS'):
         server = jenkins.Jenkins(url=url, username=app.config.get('JENKINS_USER'),
                                  password=app.config.get('JENKINS_PASSWORD'))
@@ -21,7 +23,7 @@ def get_jobs(name=''):
         result += [(j.get('name', ''), j.get('url', ''), url) for j in jobs]
 
     jobs = sorted(result, key=lambda x: x[0])
-    return render_template('jobs.html.j2', jobs=jobs)
+    return render_template('jobs.html.j2', jobs=jobs, name=name)
 
 
 if __name__ == '__main__':
