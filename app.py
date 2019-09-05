3fa7f7f0f03892f028d5292a59cb72a80a30d150
Anna Holmgren <anna.holmgren@feelgood.se>
2019-09-05 10:56:10 +0200

    Job listing

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import jenkins
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+def hello_world():
+    return "Yeah, hi"
+
+
+@app.route('/jobs')
+def get_jobs():
+    result = list()
+    for url in app.config.get('JENKINS_URLS'):
+        server = jenkins.Jenkins(url=url, username=app.config.get('JENKINS_USER'),
+                                 password=app.config.get('JENKINS_PASSWORD'))
+        jobs = server.get_jobs()
+        result += [(j.get('name', ''), j.get('url', ''), url) for j in jobs]
+
+    jobs = sorted(result, key=lambda x: x[0])
+    return render_template('jobs.html.j2', jobs=jobs)
+
+
+if __name__ == '__main__':
+    app.run(debug=1)
---
+++ config.skeleton.py
@@ -0,0 +1,4 @@
+JENKINS_USER = "replace me"
+JENKINS_PASSWORD = "so secret"
+JENKINS_URLS = ["http://jenkins-url:port"]
+
